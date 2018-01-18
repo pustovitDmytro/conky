09128998287f8254cb65a94a2ba32e1a51082030
Dmytro Pustovit <dipustovit@gmail.com>
2018-01-18 19:46:49 +0200

    updated via lsbInitScript

--- conkystartup.sh
+++ conkystartup.sh
@@ -1,3 +1,12 @@
-#!/bin/bash
-# sleep 10
-conky -d -q -c .conkyrc &
+#!/bin/sh
+
+### BEGIN INIT INFO
+# Provides: conky-widget
+# Required-Start: $local_fs $network
+# Required-Stop: $local_fs $network
+# Default-Start: 2 3 4 5
+# Default-Stop: 0 1 6
+# Short-Description: Conky widget
+### END INIT INFO
+
+conky -d -q -c ~/.config/conky/.conkyrc &
--- install.sh
+++ install.sh
@@ -8,14 +8,20 @@ rm -f ~/.config/conkystartup.sh
 cp .conkyrc ~/.config/conky/.conkyrc
 cp conkystartup.sh ~/.config/conky/conkystartup.sh
 
-echo 'files were copied to ~/.config/conky'
-echo 'to start conky now try'
-echo -e 'conky -c ~/.config/conky\n'
+# echo 'files were copied to ~/.config/conky'
+# echo 'to start conky now try'
+# echo -e 'conky -c ~/.config/conky\n'
 
-cd ~/.config/conky
-chmod a+x conkystartup.sh
+# cd ~/.config/conky
+cp conkystartup.sh /etc/init.d/conkyCustom
 
-echo 'to add conky to startUp add line'
-echo 'sh ~/.config/conky/conkystartup.sh'
-echo 'to file: /etc/rc.local'
-echo 'before line exit 0'
+chmod a+x /etc/init.d/conkyCustom
+
+
+update-rc.d conkyCustom defaults
+
+echo 'done'
+# echo 'to add conky to startUp add line'
+# echo 'sh ~/.config/conky/conkystartup.sh'
+# echo 'to file: /etc/rc.local'
+# echo 'before line exit 0'
