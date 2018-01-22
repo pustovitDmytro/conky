77640b6da5547ac20e624d586f4f440421e69930
Dmytro Pustovit <dipustovit@gmail.com>
2018-01-22 12:31:57 +0200

    all in startup

--- conkystartup.sh
+++ conkystartup.sh
@@ -2,11 +2,13 @@
 
 ### BEGIN INIT INFO
 # Provides: conky-widget
-# Required-Start: $local_fs $network
+# Required-Start: $all
 # Required-Stop: $local_fs $network
 # Default-Start: 2 3 4 5
 # Default-Stop: 0 1 6
 # Short-Description: Conky widget
 ### END INIT INFO
 
-conky -d -q -c ~/.config/conky/.conkyrc &
+echo "Try run conky" >> /var/log/test.log
+sleep 10
+conky -d -q -c ~/.config/conky/.conkyrc >> /var/log/test.log &
