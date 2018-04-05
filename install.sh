6727682150770c334db9df72fa36d3e66a27d12a
dmytroPust <dipustovit@gmail.com>
2018-04-05 14:08:53 +0300

    new install script

--- install.sh
+++ install.sh
@@ -1,35 +1,15 @@
 #!/bin/bash
 set -e
 
-CONFIG_DIR=~/.config/conky
-SRC_DIR='autostart'
+sudo apt install conky-all
 
-if [ -d $CONFIG_DIR ]; then
-    echo $CONFIG_DIR 'not empty'
-    rm -rf $CONFIG_DIR
-    echo $CONFIG_DIR 'burned to the ground'
-fi
+CONFIG_DIR=/etc
+SRC_DIR='src'
 
-mkdir -p ${CONFIG_DIR}
+cp ${SRC_DIR}/.conkyrc ${CONFIG_DIR}/.conkyrc
 
-cp .conkyrc ${CONFIG_DIR}/.conkyrc
-cp ${SRC_DIR}/conkyWidget.desktop ${CONFIG_DIR}/conkyWidget.desktop-example
-cp ${SRC_DIR}/.xinitrc ${CONFIG_DIR}/.xinitrc-example
-cp ${SRC_DIR}/conky.png ${CONFIG_DIR}/icon.png
-
-echo 'files were copied to' $CONFIG_DIR
+echo '.conkyrc was copied to' $CONFIG_DIR
 cp ${SRC_DIR}/conkyWidget.desktop ~/.config/autostart
 echo 'conkyWidget.desktop was copied to ~/.config/autostart'
-
-if [ ! -f ~/.xinitrc ]; then
-    cp ${SRC_DIR}/.xinitrc ~/.xinitrc
-    chmod +x ~/.xinitrc
-else echo 'file ~/.xinitrc exist'
-fi
-
-if [ ! -f ~/.xsession ]; then
-    ln -s ~/.xinitrc ~/.xsession
-else echo 'file ~/.xsession exist, can not create symlink'
-fi
 
 echo 'done'
